09fde8859694e7b4b6377ca143a12cc39641de22
MosNes <7hekarl@gmail.com>
2022-12-11 12:25:14 -0500

    added examples of Options API, Composition API with setup function, and Composition API with Script Setup pattern

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,83 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div>
+      <button class="btn" @click="decreaseCounter">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button class="btn" @click="increaseCounter">+</button>
+    </div>
   </div>
 </template>
+
+<!-- OPTIONS API------------------------ -->
+<!-- <script>
+export default {
+
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+
+  methods: {
+    increaseCounter() {
+      this.counter++;
+    },
+    decreaseCounter() {
+      this.counter--;
+    }
+  },
+}
+</script> -->
+
+<!-- COMPOSITION API WITH SETUP FUNCTION------------------------ -->
+<!-- <script>
+  import {ref} from 'vue';
+  export default {
+    setup() {
+      const counter = ref(0);
+
+      const increaseCounter = () => {
+        counter.value++;
+      }
+
+      const decreaseCounter = () => {
+        counter.value--;
+      }
+
+      return {
+        counter,
+        increaseCounter,
+        decreaseCounter
+      }
+    }
+  }
+</script> -->
+
+<!-- COMPOSITION API WITH SCRIPT SETUP PATTERN (RECOMMENDED)------------------------ -->
+<script setup>
+import { ref } from 'vue';
+
+//all code for counter
+const counter = ref(0);
+
+const increaseCounter = () => {
+  counter.value++;
+}
+
+const decreaseCounter = () => {
+  counter.value--;
+}
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
