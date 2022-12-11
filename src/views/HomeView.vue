cc4e8f1302fd647a5f64a670b2db8f31e36b7d17
MosNes <7hekarl@gmail.com>
2022-12-11 13:20:57 -0500

    changed counter from ref to reactive object

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="home">
+
+    <h3>{{ counterData.title }}:</h3>
+
     <div>
       <button class="btn" @click="decreaseCounter">-</button>
-      <span class="counter">{{ counter }}</span>
+      <span class="counter">{{ counterData.count }}</span>
       <button class="btn" @click="increaseCounter">+</button>
     </div>
+
+      <div class="edit">
+        <h4>Edit Counter Title</h4>
+        <input type="text" v-model="counterData.title">
+      </div>
   </div>
 </template>
+
+<!-- COMPOSITION API WITH SCRIPT SETUP PATTERN (RECOMMENDED)------------------------ -->
+<script setup>
+import { ref, reactive } from 'vue';
+
+//all code for counter
+const counterData = reactive({
+  count: 0,
+  title: 'My Counter'
+})
+
+const increaseCounter = () => {
+  counterData.count++;
+}
+
+const decreaseCounter = () => {
+  counterData.count--;
+}
+
+</script>
 
 <!-- OPTIONS API------------------------ -->
 <!-- <script>
@@ -53,21 +81,7 @@ export default {
   }
 </script> -->
 
-<!-- COMPOSITION API WITH SCRIPT SETUP PATTERN (RECOMMENDED)------------------------ -->
-<script setup>
-import { ref } from 'vue';
 
-//all code for counter
-const counter = ref(0);
-
-const increaseCounter = () => {
-  counter.value++;
-}
-
-const decreaseCounter = () => {
-  counter.value--;
-}
-</script>
 
 <style>
 .home {
@@ -79,5 +93,8 @@ const decreaseCounter = () => {
 .counter {
   font-size: 40px;
   margin: 10px;
+},
+.edit {
+  margin-top: 60px;
 }
 </style>
